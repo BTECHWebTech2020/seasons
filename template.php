926b7605d0df2d5e6c6b92c5e67f1c64320f8643
Eva Poulsen <eva@btech.au.dk>
2020-11-02 12:50:42 +0100

    Update template.php

--- template.php
+++ template.php
@@ -98,7 +98,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/jquery-3.5.1.js"></script>
     <script>window.jQuery || document.write('<script src="js/jquery-slim.js"><\/script>')</script>
     <script src="js/popper.js"></script>
     <script src="js/bootstrap.js"></script>
